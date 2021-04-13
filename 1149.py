58ab6e0cab67aaf16ae1c0c25b9dd93b72d65471
harry595 <harry595@naver.com>
2021-04-14 01:17:21 +0900

    1149 RGB

--- 1149.py
+++ 1149.py
@@ -1,8 +1,13 @@
-# https://www.acmicpc.net/problem/11726 2×n 타일링 문제
-# sol(1)=1 sol(2)=2 sol(3)=3 sol(4)=5
+# https://www.acmicpc.net/problem/1149 RGB거리 문제
+# 전의 집과 겹치면 안되는 것만 확인
+# 2차원 배열 형식으로 모든 집들에 전 집들의 cost를 추가하여 계산
 
 N = int(input())
-cost_per_home = []
+cost= []
 for _ in range(N):
-    cost_per_home.append(list(map(int, input().split())))
-    
+    cost.append(list(map(int, input().split())))
+for i in range(1,len(cost)):
+    cost[i][0]=min(cost[i-1][1],cost[i-1][2])+cost[i][0]
+    cost[i][1]=min(cost[i-1][0],cost[i-1][2])+cost[i][1]
+    cost[i][2]=min(cost[i-1][0],cost[i-1][1])+cost[i][2]
+print(min(cost[N-1][0],cost[N-1][1],cost[N-1][2]))
